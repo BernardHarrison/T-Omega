0949ef4c667a97a59968442965b64ee7a1711e8c
BernardHarrison <bernie@socketlabs.com>
2020-03-07 13:37:58 -0500

    Working on new model and sample layout

--- Site/src/app/stores/condition-builder-store/index.ts
+++ Site/src/app/stores/condition-builder-store/index.ts
@@ -1,3 +1,24 @@
-export interface ConditionDefinition {
 
+export interface conditionTest {
+	subject: string; //The x-path to the data field
+	op: string;      //The x-path to the operator
+	val: any;        //The value to comapre
+}
+
+export enum conditionTestGroupTypes {
+	and,
+	or
+}
+
+export class conditionTestGroup {
+	"op": conditionTestGroupTypes;
+	"tests": conditionTest[];
+	"groups": conditionTestGroup[];
+}
+
+
+export interface ConditionDefinition {
+	id: number;
+	name: string;
+	condition: conditionTestGroup;
 }
